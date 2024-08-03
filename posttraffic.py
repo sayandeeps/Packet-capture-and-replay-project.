a5c141e4358f2e0613b71f4143c180500c92220b
Sayandeep <sayandeep11@gmail.com>
2024-08-03 19:26:46 +0530

    gentraffic replay traffic refine

--- posttraffic.py
+++ posttraffic.py
@@ -2,24 +2,24 @@ import socket
 import ssl
 
 def send_post_request(ip, port, request):
-    # Create a socket object
+
     client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-    # Wrap the socket with SSL to handle HTTPS
+
     if port == 443:
         context = ssl.create_default_context()
         client_socket = context.wrap_socket(client_socket, server_hostname=ip)
 
     try:
-        # Connect to the server
+
         client_socket.connect((ip, port))
         print("Connected to server")
 
-        # Send the HTTP POST request
+  
         client_socket.sendall(request.encode())
         print("Request sent")
 
-        # Receive the response
+
         response = b""
         while True:
             part = client_socket.recv(4096)
@@ -30,17 +30,16 @@ def send_post_request(ip, port, request):
     except Exception as e:
         print(f"An error occurred: {e}")
     finally:
-        # Close the socket
+    
         client_socket.close()
         print("Connection closed")
 
     return response.decode()
 
-# Define the IP, port, and POST request
 ip = '76.76.21.22'
 port = 443
 
-# Construct the HTTP POST request
+
 http_request = (
     "POST /api/send HTTP/1.1\r\n"
     f"Host: {ip}\r\n"
@@ -51,10 +50,8 @@ http_request = (
     '{"email": "sayandeep11@gmail.com", "subject": "efwfe", "message": "ewfqwefw ef wef ew few fwe fef"}'
 )
 
-# Send the request and get the response
 response = send_post_request(ip, port, http_request)
 
-# Dump the response into a file
 with open('response_send_api.txt', 'w') as file:
     file.write(response)
 
--- replay.py
+++ replay.py
@@ -1,8 +1,6 @@
 from scapy.all import rdpcap, sendp
 
-# Load the captured packets
 packets = rdpcap("telaverge.pcap")
 
-# Replay packets
 for packet in packets:
-    sendp(packet, iface="en0")  # Send on the specified network interface
+    sendp(packet, iface="en0")  
